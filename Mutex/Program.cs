90c6c00c4574ddba31584eb6c68e4ca2616b92b5
Nabil Shuhaiber <nabil@shuhaiber.com>
2015-02-15 12:42:10 +0000

    Initial commit

---
+++ Mutex/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Mutex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var mutex = new System.Threading.Mutex(true, "App-Startup"))
+            {
+                Console.WriteLine("Starting up...");
+                Thread.Sleep(TimeSpan.FromSeconds(10));
+                mutex.ReleaseMutex();
+            }
+
+            RunProgram();
+        }
+
+        static void RunProgram()
+        {
+            Console.WriteLine("Running. Press Enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start(args[0]);
+
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+
+            using (var mutex = Mutex.OpenExisting("App-Startup"))
+            {
+                try
+                {
+                    if (!mutex.WaitOne(TimeSpan.FromSeconds(60), false))
+                    {
+                        throw new Exception("There was a problem with App initialisation. Waited 1 minute. Exiting.");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                
+                Console.WriteLine("App started. Start testing..");
+
+                for (int i = 0; i < 1000000; i++)
+                {
+                    Console.WriteLine("Testing... " + i);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
